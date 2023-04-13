482e0a71d2c456f72c2646be0e975ed4a3b15990
YoonJihwan <jecgdd@naver.com>
2023-04-13 21:52:59 +0900

    * axios, dayjs 기본 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,17 @@
 import type { AppProps } from "next/app";
 
+// dayjs
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+
+// utils
+import setAxiosInterceptors from "utils/setAxiosInterceptors";
+
+dayjs.extend(timezone);
+dayjs.tz.setDefault("Asia/Seoul"); // 타임존 설정
+
+setAxiosInterceptors();
+
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
 };
